2d34abafc990a580ea303cc09c60642bef632ca4
elvan <elvankun@gmail.com>
2020-01-13 13:46:33 +0700

    boiler-plate first commit

---
+++ src/containers/DefaultContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>container</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultContainer'
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/containers/DefaultHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Header</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultHeader'
+}
+</script>
+
+<style scoped>
+
+</style>
