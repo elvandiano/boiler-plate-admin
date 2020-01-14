9892396d596c9cf58480a66144b9a7e172f91797
Pawitra Warda <pawitrawarda@gmail.com>
2020-01-14 13:55:35 +0700

    bugfix default container add playground component for making component

--- src/views/ComponentPlayground.vue
+++ src/views/ComponentPlayground.vue
@@ -1,12 +1,10 @@
 <template>
-  <div>
-    <h1>container</h1>
-  </div>
+  <div></div>
 </template>
 
 <script>
 export default {
-  name: 'DefaultContainer'
+  name: 'ComponentPlayground'
 }
 </script>
 
