7ac23190dd681779575ffb24a3156977ab4758d0
Pawitra Warda <pawitrawarda@gmail.com>
2020-01-17 18:26:40 +0700

    add loader

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.scss'
 import BootstrapVue from 'bootstrap-vue'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
+Vue.use(Loading)
 
 new Vue({
   router,
