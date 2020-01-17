63be66a01323afba5e49bca152fe845bba77d691
Pawitra Warda <pawitrawarda@gmail.com>
2020-01-17 13:44:51 +0700

    add form and button

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,3 @@
     <router-view/>
   </div>
 </template>
-
-<style lang="scss">
-// Import Main styles for this application
-@import 'assets/sass/view';
-
-// Import Boostrap Styles for this application
-@import '~bootstrap/scss/bootstrap';
-@import '~bootstrap-vue/src/index.scss';
-
-// Import Bootstrap Vue Styles
-@import '~bootstrap-vue/dist/bootstrap-vue.css';
-
-// Import CoreUI
-@import "~@coreui/coreui/scss/coreui";
-
-// Import Font Awesome Icons Set
-$fa-font-path: '~font-awesome/fonts/';
-@import '~font-awesome/scss/font-awesome.scss';
-
-</style>
