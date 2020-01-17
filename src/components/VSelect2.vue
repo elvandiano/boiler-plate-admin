285194216843f0ce2db706f7c2e853a4d66ca63b
Pawitra Warda <pawitrawarda@gmail.com>
2020-01-17 10:17:14 +0700

    add vue-select

---
+++ src/components/VSelect2.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <v-select :options="data"></v-select>
+    </div>
+</template>
+
+<script>
+import vSelect from 'vue-select'
+export default {
+  name: 'VSelect2',
+  props: ['data'],
+  components: {
+    vSelect
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
