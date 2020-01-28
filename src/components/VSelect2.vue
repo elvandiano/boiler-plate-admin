cfa672ff46c626ead7216b501486347229fafff4
Pawitra Warda <pawitrawarda@gmail.com>
2020-01-28 10:03:35 +0700

    add validation form

--- src/components/VSelect2.vue
+++ src/components/VSelect2.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-      <v-select ref="vSelect" :value="value" @input="setSelectedInput($event)" :options="data" :placeholder="placeholder" :searchable="searchable"></v-select>
+      <v-select :class="stateData ? '' : 'error'" ref="vSelect" :value="value" @input="setSelectedInput($event)" :options="data" :placeholder="placeholder" :searchable="searchable">
+
+      </v-select>
+      <span v-show="!state" class="error-message">{{ errorMessage }}</span>
     </div>
 </template>
 
@@ -8,6 +11,11 @@
 import vSelect from 'vue-select'
 export default {
   name: 'VSelect2',
+  data () {
+    return {
+      stateData: true
+    }
+  },
   props: {
     'data': Array,
     'placeholder': String,
@@ -15,16 +23,16 @@ export default {
       type: Boolean,
       default: true
     },
-    'value': String
+    'value': String,
+    'state': {
+      type: Boolean,
+      default: true
+    },
+    'errorMessage': String
   },
-  computed: {
-    getSelectedVal: {
-      get () {
-        return this.selectedVal
-      },
-      set (val) {
-        return val
-      }
+  watch: {
+    state: function () {
+      this.stateData = this.state
     }
   },
   methods: {
